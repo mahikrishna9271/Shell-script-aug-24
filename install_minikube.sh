a779584f81c0eee710c8ad60dd9fbded0ac59d52
Ubuntu <ubuntu@ip-172-31-9-193.ec2.internal>
2024-07-15 16:12:54 +0000

    added minikube server script

---
+++ install_minikube.sh
@@ -0,0 +1,5 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+sudo usermod -aG docker ubuntu
+sudo newgrp docker
+minikube start --driver=docker
